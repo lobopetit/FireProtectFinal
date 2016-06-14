4d3b461db42095d3e4fe3362cdfec5a5f3752cf7
lobopetit <xavierbonell@gmail.com>
2016-06-14 17:33:29 +0200

    Projecte final

---
+++ app/src/main/java/com/example/xavier/fireprotect/gcm/QuickstartPreferences.java
@@ -0,0 +1,11 @@
+package com.example.xavier.fireprotect.gcm;
+
+/**
+ * Created by xavier on 14/06/16.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
